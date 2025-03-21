2dd6e2c50fdeb068334abfbe4f786df4a774b317
Woguem <woguemyen@gmail.com>
2025-03-21 13:22:21 +0100

    Modify script

--- RAG_and_FAISS.py
+++ RAG_and_FAISS.py
@@ -1,3 +1,8 @@
+"""
+@author: Dr Yen Fred WOGUEM 
+
+"""
+
 
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
@@ -12,8 +17,9 @@ import os
 
 
 
-# Charger les documents (.txt et .pdf)
-# Lister tous les fichiers .txt et .pdf
+
+# List of all .txt and .pdf files
+
 folder_path = './'
 
 txt_files = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
@@ -21,33 +27,33 @@ pdf_files = [f for f in os.listdir(folder_path) if f.endswith('.pdf')]
 
 documents = []
 
-# Charger tous les fichiers .txt
+# Load all .txt files
 for txt_file in txt_files:
     txt_path = os.path.join(folder_path, txt_file)
     loader_txt = TextLoader(txt_path, encoding="utf-8")
-    documents.extend(loader_txt.load())  # Ajouter le contenu de chaque fichier texte
+    documents.extend(loader_txt.load())  
 
-# Charger tous les fichiers .pdf
+# Load all .pdf files
 for pdf_file in pdf_files:
     pdf_path = os.path.join(folder_path, pdf_file)
     loader_pdf = PyPDFLoader(pdf_path)
-    documents.extend(loader_pdf.load())  # Ajouter le contenu de chaque fichier PDF
+    documents.extend(loader_pdf.load())  
 
-# Diviser les documents (.txt et .pdf) en petits morceaux
+# Divide documents (.txt and .pdf) into smaller pieces
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 docs = text_splitter.split_documents(documents)
 
 
-# Configuration du modèle de recherche
+# Search model configuration
 embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-# Créer l’index FAISS à partir des documents
+# Create the FAISS index from documents
 faiss_index = FAISS.from_documents(docs, embedding)
 
-# Sauvegarder l'index pour un usage futur
+# Save index for future use
 faiss_index.save_local("mon_index_faiss")
 
-# Charger l'index FAISS
+# Loading the FAISS index
 faiss_index = FAISS.load_local("mon_index_faiss", embeddings=embedding, allow_dangerous_deserialization=True) # FAISS : Facebook AI Similarity Search
 
 retrieved_docs = faiss_index.similarity_search("What are the latest regulatory updates on insurance?", k=3)
@@ -58,18 +64,18 @@ for doc in retrieved_docs:
 
   
 
-# Charger un pipeline Hugging Face
+# Loading a Hugging Face pipeline
 generator = pipeline("text-generation", model="gpt2", device_map="auto", max_length=500, truncation=True)
 
-# Créer un LLM LangChain
+# Create an LLM LangChain
 llm = HuggingFacePipeline(pipeline=generator)
 
-# Configuration du générateur RAG
+# RAG generator configuration
 qa_chain = RetrievalQA.from_chain_type(
     llm=llm, retriever=faiss_index.as_retriever()
 )
 
-# Interrogation de la base de documents
+# Querying the document database
 query = "What are the latest regulatory updates on insurance?"
 response = qa_chain.invoke(query)
 
